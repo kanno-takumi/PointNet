f6b271626ddfc90d978d5cd28e847528398ed73b
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-12-04 18:32:26 +0900

    json形式でメタデータを作成

--- install3Dmodel.py
+++ install3Dmodel.py
@@ -22,13 +22,26 @@ tf.random.set_seed(12)
 # shapenetデータセットの一部であるPASCAL 3D+をダウンロード
 dataset_url = "https://media.githubusercontent.com/media/kaz12tech/datasets/main/shapenet.zip?download=true"
 
-dataset_path = keras.utils.get_file(
-    fname="shapenet.zip",
-    origin=dataset_url,
-    cache_subdir="datasets",
-    hash_algorithm="auto",
-    extract=True,
-    archive_format="auto",
-    cache_dir="datasets",
-)
-print('dataset path:', dataset_path)
+# dataset_path = keras.utils.get_file(
+#     fname="shapenet.zip",
+#     origin=dataset_url,
+#     cache_subdir="datasets",
+#     hash_algorithm="auto",
+#     extract=True,
+#     archive_format="auto",
+#     cache_dir="datasets",
+# )
+# print('dataset path:', dataset_path)
+
+#printで確認したpath
+dataset_path = "/tmp/.keras/datasets/shapenet.zip"
+
+dirname = os.path.dirname(dataset_path)
+meta_path = 'PartAnnotation/metadata.json'
+
+# メタデータのロード
+with open( os.path.join(dirname, meta_path) ) as json_file:
+  metadata = json.load(json_file)
+
+# 整形して表示
+pprint.pprint(metadata)
