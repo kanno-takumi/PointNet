73b0309d567cdcecb5a6b383d8e7c52172d4e6b6
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-12-06 15:36:41 +0900

    更新

--- dataset2pointcloud.py
+++ dataset2pointcloud.py
@@ -26,22 +26,36 @@ tf.random.set_seed(12)
 
 
 NUM_SAMPLE_POINTS = 1024  # サンプリング数
-dataset_path = "/tmp/.keras/datasets"
+dataset_path = "~/python/dataset_pointnet"
 dirname = os.path.dirname(dataset_path)
-obj_dir = "02691156" #サンプルとして一つ挙げてみる。
+meta_path = 'PartAnnotation/metadata.json'
+with open( os.path.join(dirname, meta_path) ) as json_file:
+  metadata = json.load(json_file)
+
+target = 'Chair'
+obj_dir = metadata[target]['directory']
 points_dir = os.path.join(dirname, 'PartAnnotation', obj_dir, 'points')
 points_files = glob(os.path.join(points_dir,"*.pts"))
 
 #まずは1つのファイルから開く。
-coords = []
-for point_file in tqdm(points_files): #複数のファイルから、
-    # point_cloud = np.loadtxt(point_file)
-    with open(point_file,'r') as f:
-        pt_list = f.read().split('\n')
-    if pt_list.shape[0] < NUM_SAMPLE_POINTS:
-        continue
+# coords = []
+# for point_file in tqdm(points_files): #複数のファイルから、
+#     # point_cloud = np.loadtxt(point_file)
+#     with open(point_file,'r') as f:
+#         pt_list = f.read().split('\n')
+        
+#     if pt_list.shape[0] < NUM_SAMPLE_POINTS:
+#         continue
+point_clouds_np = []
+for point_file in tqdm(points_files):
+  point_cloud_np = np.loadtxt(point_file)
+  if point_cloud_np.shape[0] < NUM_SAMPLE_POINTS:
+    continue
+point_clouds = point_clouds_np.tolist()
+
+
     
-data_to_save = {'coords':coords}
+data_to_save = {'coords':point_clouds}
 
 file_name = 'dataset/pointcloud/sample.json'
 with open(file_name,'w') as file:
